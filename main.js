f2d9e8639ffadd29a7a262e307ae72936480419e
Thiagovix <thiago.pagoto58@gmail.com>
2024-08-20 17:09:05 -0300

    alterado

--- main.js
+++ main.js
@@ -2,17 +2,17 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-// menuBtn.addEventListener("click", () => {
-//   navLinks.classList.toggle("open");
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
 
-//   const isOpen = navLinks.classList.contains("open");
-//   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-// });
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
+});
 
-// navLinks.addEventListener("click", () => {
-//   navLinks.classList.remove("open");
-//   menuBtnIcon.setAttribute("class", "ri-menu-line");
-// });
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
 const scrollRevealOption = {
   distance: "50px",
